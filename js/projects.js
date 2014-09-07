37227af5871306b06ea5393cfaacc94809fb238a
Andrew Hyder <hyd415@gmail.com>
2014-09-07 14:27:00 -0700

    Basic project page

---
+++ js/projects.js
@@ -0,0 +1,25 @@
+var cfapi_url = 'http://codeforamerica.org/api/organizations/Code-for-San-Francisco/projects';
+
+// Go get projects! Then show them off.
+$.getJSON(cfapi_url, showProjects);
+
+function showProjects(response){
+
+  projects = response.objects;
+
+  // loop through our project data
+  $.each(projects, function(i, project){
+
+    // to display text like 'x days ago' we use moment.js's awesome fromNow function
+    // http://momentjs.com/docs/#/displaying/fromnow/
+    project['last_updated_formatted'] = moment(project['last_updated']).fromNow();
+
+  });
+  $("#hack-night-projects tbody").append(ich.projects({projects:projects}));
+
+  // Follow next page links
+  if (response.pages.next) {
+    $.getJSON(response.pages.next, showProjects);
+  }
+//
+}
