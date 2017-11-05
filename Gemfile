0bf9b363571f75439e913df01e6527223565a33e
Jesse Szwedko <jesse.szwedko@gmail.com>
2017-11-05 13:27:27 -0800

    Update Ruby and github-pages versions
    Fix some syntax errors in the templates

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
-ruby "2.2.2"
+ruby "2.4.2"
 
 gem 'github-pages'
--- js/projectData.js
+++ js/projectData.js
@@ -2,7 +2,8 @@
 ---
 var projectData =
 {
-{% for post in site.posts reversed | sort: title %}
+{% assign posts = site.posts | sort: title | reversed %}
+{% for post in posts %}
 {% if post.layout == 'projects' %}
 {% assign id_array = post.path | split:"/" %}
   	"{{id_array[2]}}": {
